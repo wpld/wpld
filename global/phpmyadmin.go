9d17089c91a6e7f4ceeaf1c6f0060bed391650d8
Eugene Manuilov <eugene.manuilov@gmail.com>
2021-01-20 10:12:07 +0200

    Update PMA to autologin and allow for multiple servers.

--- global/phpmyadmin.go
+++ global/phpmyadmin.go
@@ -1,7 +1,10 @@
 package global
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
+	"fmt"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
@@ -12,6 +15,35 @@ const (
 	MYADMIN_IMAGE_NAME = "phpmyadmin:latest"
 	MYADMIN_CONTAINER_NAME = "wpld_global_phpmyadmin"
 )
+
+func getBase64EncodedPMAConfig() string {
+	servers := []map[string]string{
+		{
+			"host": MYSQL_CONTAINER_NAME,
+			"label": "global",
+		},
+	}
+
+	buffer := bytes.NewBufferString(`
+<?php
+
+$cfg['blowfish_secret'] = 'l3+wF5o$MUK@hj;[HLkQ4#V9-m?b4JmgXa]H_{uH#H]x|oQI%c1s|wFOGTc[<{3M';
+$cfg['ServerDefault']   = 1;
+`)
+
+	for i, server := range servers {
+		config := `
+$cfg['Servers'][%[1]d]['host']      = '%[2]s';
+$cfg['Servers'][%[1]d]['auth_type'] = 'config';
+$cfg['Servers'][%[1]d]['user']      = 'root';
+$cfg['Servers'][%[1]d]['password']  = 'password';
+$cfg['Servers'][%[1]d]['verbose']   = '%[3]s';
+`
+		buffer.WriteString(fmt.Sprintf(config, i + 1, server["host"], server["label"]))
+	}
+
+	return base64.StdEncoding.EncodeToString(buffer.Bytes())
+}
 
 func RunMyAdmin(ctx context.Context, cli *client.Client, pull bool) error {
 	img := utils.Image{
@@ -29,7 +61,7 @@ func RunMyAdmin(ctx context.Context, cli *client.Client, pull bool) error {
 		Create: &container.Config{
 			Image: img.Name,
 			Env: []string{
-				"PMA_HOST=" + MYSQL_CONTAINER_NAME,
+				"PMA_USER_CONFIG_BASE64=" + getBase64EncodedPMAConfig(),
 				"UPLOAD_LIMIT=1024MiB",
 			},
 		},
