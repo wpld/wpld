2eb96140b113416d6272a93d48eac4c127b005a3
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-07-11 18:03:14 +0300

    Added nginx service.

--- internal/cases/new-project.go
+++ internal/cases/new-project.go
@@ -74,7 +74,50 @@ func newProjectPromptPipe() pipelines.Pipe {
 		}
 
 		projectSlug := slug.Make(answers.Name)
+
 		wpVolume := fmt.Sprintf("%s__wp", projectSlug)
+		dbVolume := fmt.Sprintf("%s__db", projectSlug)
+
+		wp := entities.Service{
+			Name:  "WordPress",
+			Image: fmt.Sprintf("wordpress:5-php%s-fpm-alpine", answers.PHP),
+			Volumes: []string{
+				fmt.Sprintf("%s:/var/www/html", wpVolume),
+			},
+			Env: map[string]string{
+				"WORDPRESS_DB_HOST":     "db",
+				"WORDPRESS_DB_USER":     "wordpress",
+				"WORDPRESS_DB_PASSWORD": "password",
+				"WORDPRESS_DB_NAME":     projectSlug,
+			},
+		}
+
+		db := entities.Service{
+			Name:  "Database",
+			Image: "mariadb:latest",
+			Volumes: []string{
+				dbVolume,
+			},
+			Env: map[string]string{
+				"MYSQL_DATABASE":           projectSlug,
+				"MYSQL_USER":               "wordpress",
+				"MYSQL_PASSWORD":           "password",
+				"MYSQL_INITDB_SKIP_TZINFO": "skip",
+			},
+		}
+
+		nginx := entities.Service{
+			Name:  "Nginx",
+			Image: "nginx:alpine",
+			Ports: []string{
+				"80:80",
+				"443:443",
+			},
+			Volumes: []string{},
+			VolumesFrom: []string{
+				"wp",
+			},
+		}
 
 		return next(context.WithValue(
 			ctx,
@@ -84,34 +127,12 @@ func newProjectPromptPipe() pipelines.Pipe {
 				Domains: answers.Domains,
 				Volumes: []string{
 					wpVolume,
+					dbVolume,
 				},
 				Services: map[string]entities.Service{
-					"wp": {
-						Name:  "WordPress",
-						Image: fmt.Sprintf("wordpress:5-php%s-fpm-alpine", answers.PHP),
-						Volumes: []string{
-							fmt.Sprintf("%s:/var/www/html", wpVolume),
-						},
-						Env: map[string]string{
-							"WORDPRESS_DB_HOST":     "db",
-							"WORDPRESS_DB_USER":     "wordpress",
-							"WORDPRESS_DB_PASSWORD": "password",
-							"WORDPRESS_DB_NAME":     projectSlug,
-						},
-					},
-					"db": {
-						Name:  "Database",
-						Image: "mariadb:latest",
-						Ports: []string{
-							"3306:3306",
-						},
-						Env: map[string]string{
-							"MYSQL_DATABASE":           projectSlug,
-							"MYSQL_USER":               "wordpress",
-							"MYSQL_PASSWORD":           "password",
-							"MYSQL_INITDB_SKIP_TZINFO": "skip",
-						},
-					},
+					"wp":    wp,
+					"db":    db,
+					"nginx": nginx,
 				},
 			},
 		))
