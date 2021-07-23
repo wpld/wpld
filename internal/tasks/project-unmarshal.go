dd474e3220b6dca53ecb3ab4a58c0f32dae9fceb
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-07-23 12:13:15 +0300

    Added ability to find up config file.

--- internal/tasks/project-unmarshal.go
+++ internal/tasks/project-unmarshal.go
@@ -2,6 +2,8 @@ package tasks
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/afero"
 	"gopkg.in/yaml.v3"
@@ -12,13 +14,40 @@ import (
 
 func ProjectUnmarshalPipe(fs afero.Fs) pipelines.Pipe {
 	return func(ctx context.Context, next pipelines.NextPipe) error {
-		data, err := afero.ReadFile(fs, ".wpld.yml")
+		var config string
+
+		dir, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		for {
+			config = filepath.Join(dir, ".wpld.yml")
+			if exists, err := afero.Exists(fs, config); err != nil {
+				return err
+			} else if exists {
+				break
+			}
+
+			parent := filepath.Dir(dir)
+			if parent == dir {
+				break
+			} else {
+				dir = parent
+			}
+		}
+
+		data, err := afero.ReadFile(fs, config)
 		if err != nil {
 			return err
 		}
 
 		var project entities.Project
 		if err = yaml.Unmarshal(data, &project); err != nil {
+			return err
+		}
+
+		if err := os.Chdir(dir); err != nil {
 			return err
 		}
 
