9d6dd8d76ea2ada52ee96bb55a25fdb3f9b3c31f
Eugene Manuilov <eugene.manuilov@get10up.com>
2021-08-02 10:57:20 +0300

    Fixed missing dnsmasq image tag issue.

--- internal/entities/services/dns.go
+++ internal/entities/services/dns.go
@@ -8,7 +8,7 @@ func NewDnsService() entities.Service {
 	return entities.Service{
 		ID: "wpld__dnsmasq",
 		Spec: entities.Specification{
-			Image: "4km3/dnsmasq",
+			Image: "4km3/dnsmasq:latest",
 			CapAdd: []string{
 				"NET_ADMIN",
 			},
